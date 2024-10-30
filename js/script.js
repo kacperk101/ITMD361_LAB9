851e72b1759f3d731cf7780593ba7133d97ed109
Kacper K <kacper3525@yahoo.com>
2024-10-29 20:20:25 -0500

    Added function to show text
    Added a function which grabs entryinput ID and then appends my name to the textbox once a message is entered. Added event listener on button so when clicked it displays the text entered by user.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 function init(){
    var button = document.getElementById('entrybutton');
 
+ function showMeText(){
+    var textbox = document.getElementById('entryinput');
+	document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Kacper Kraj: " + textbox.value);
+  }
 
 
-
-
-
+ button.addEventListener('click', showMeText);
+  }
 
 
 window.addEventListener('load', init);
